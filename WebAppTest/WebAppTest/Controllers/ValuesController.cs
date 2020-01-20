da7bc54c68e6379c3e764785aaad45311e55f993
Kurt Monge <kurt.monge@knowit.com>
2020-01-20 22:58:26 +0100

    Initial checkin

---
+++ WebAppTest/WebAppTest/Controllers/ValuesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppTest.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return $"Hello from : {System.Environment.MachineName}.";
+        }
+    }
+}
